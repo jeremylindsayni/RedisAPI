c3a82334e71d3c427698851428357969d1ce8af1
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-11-17 23:12:54 +0000

    Initial commit

---
+++ RedisConnectionFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace RedisAPI
+{
+    public class RedisConnectionFactory
+    {
+        private static readonly Lazy<ConnectionMultiplexer> Connection;
+
+        private static readonly string REDIS_CONNECTIONSTRING = "REDIS_CONNECTIONSTRING";
+
+        static RedisConnectionFactory()
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = config[REDIS_CONNECTIONSTRING];
+
+            if (connectionString == null)
+            {
+                throw new KeyNotFoundException($"Environment variable for {REDIS_CONNECTIONSTRING} was not found.");
+            }
+
+            var options = ConfigurationOptions.Parse(connectionString);
+
+            Connection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(options));
+        }
+
+        public static ConnectionMultiplexer GetConnection() => Connection.Value;
+    }
+}
---
+++ RedisDataAgent.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+
+namespace RedisAPI
+{
+    public class RedisDataAgent
+    {
+        private static IDatabase _database;
+        public RedisDataAgent()
+        {
+            var connection = RedisConnectionFactory.GetConnection();
+
+            _database = connection.GetDatabase();
+        }
+
+        public string GetStringValue(string key)
+        {
+            return _database.StringGet(key);
+        }
+
+        public void SetStringValue(string key, string value)
+        {
+            _database.StringSet(key, value);
+        }
+
+        public void DeleteStringValue(string key)
+        {
+            _database.KeyDelete(key);
+        }
+    }
+}
